60c666903f958b3259fbd21864dda8b68b7143fd
Yangchi <chenade0312@gmail.com>
2024-06-18 12:26:42 +0200

    creating the calculator

--- mobileModule00/ex02/lib/main.dart
+++ mobileModule00/ex02/lib/main.dart
@@ -1,19 +1,177 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorPage(),
+    );
+  }
+}
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String expression = '0';
+  String result = '0';
+
+  void handleButtonPress(String buttonText) {
+    print('Button pressed: $buttonText');
+
+    setState(() {
+		if (buttonText == 'AC')
+		{
+			expression = '0';
+			result = '0';
+		}
+		else if (buttonText == 'C')
+		{
+			if (expression.length > 1)
+			{
+				expression = expression.substring(0, expression.length - 1);
+			}
+			else
+			{
+				expression = '0';
+			}
+		}
+		else if (buttonText == '=')
+		{
+			result = expression;
+		}
+		else
+		{
+		if (expression == '0') 
+		{
+			expression = buttonText;
+		} 
+		else
+		{
+			expression += buttonText;
+		}
+		}
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          handleButtonPress(buttonText);
+        },
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              color: Colors.grey[300],
+              alignment: Alignment.bottomRight,
+              child: Text(
+                expression,
+                style: const TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              color: Colors.grey[200],
+              alignment: Alignment.bottomRight,
+              child: Text(
+                result,
+                style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      buildButton('7'),
+                      buildButton('8'),
+                      buildButton('9'),
+                      buildButton('/'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      buildButton('4'),
+                      buildButton('5'),
+                      buildButton('6'),
+                      buildButton('*'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      buildButton('1'),
+                      buildButton('2'),
+                      buildButton('3'),
+                      buildButton('-'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      buildButton('0'),
+                      buildButton('.'),
+                      buildButton('AC'),
+                      buildButton('+'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      buildButton('C'),
+                      buildButton('='),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
